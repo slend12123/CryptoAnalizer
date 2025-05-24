d387890ccc6ac1f2851b9323bcb652e708a46a57
slend12123 <artemsprincan945@gmail.com>
2025-05-24 22:53:19 +0200

    added encrypt method

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,8 +1,22 @@
-import java.util.List;
-import java.util.Set;
+
 
 public class Cipher {
-    private static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
+    public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : text.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                char base = 'A';
+                ch = (char) ((ch - base + shift + 26) % 26 + base);
+            } else if (Character.isLowerCase(ch)) {
+                char base = 'a';
+                ch = (char) ((ch - base + shift + 26) % 26 + base);
+            }
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
 
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,3 +1,6 @@
 public class MainApp {
-
+    public static void main() {
+        String result = Cipher.encrypt("java", 15);
+        System.out.println(result);
+    }
 }

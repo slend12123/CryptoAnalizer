d47d1c331e01d62b57bf8e4662c6bf6ff89950a7
slend12123 <artemsprincan945@gmail.com>
2025-05-25 16:57:43 +0200

    added decrypt method and try/catch

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,22 +1,45 @@
-
+import java.sql.SQLOutput;
 
 public class Cipher {
 
     public static String encrypt(String text, int shift) {
         StringBuilder result = new StringBuilder();
 
-        for (char ch : text.toCharArray()) {
-            if (Character.isUpperCase(ch)) {
-                char base = 'A';
-                ch = (char) ((ch - base + shift + 26) % 26 + base);
-            } else if (Character.isLowerCase(ch)) {
-                char base = 'a';
-                ch = (char) ((ch - base + shift + 26) % 26 + base);
+        try {
+            for (char ch : text.toCharArray()) {
+                if (Character.isUpperCase(ch)) {
+                    char base = 'A';
+                    ch = (char) ((ch - base + shift + 26) % 26 + base);
+                } else if (Character.isLowerCase(ch)) {
+                    char base = 'a';
+                    ch = (char) ((ch - base + shift + 26) % 26 + base);
+                }
+                result.append(ch);
             }
-            result.append(ch);
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e.getMessage());
         }
 
         return result.toString();
     }
 
+    public static String decrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        try {
+            for (char ch : text.toCharArray()) {
+                if (Character.isUpperCase(ch)) {
+                    char base = 'A';
+                    ch = (char) ((ch - base - shift + 26) % 26 + base);
+                } else if (Character.isLowerCase(ch)) {
+                    char base = 'a';
+                    ch = (char) ((ch - base - shift + 26) % 26 + base);
+                }
+                result.append(ch);
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+        return result.toString();
+    }
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class MainApp {
-    public static void main() {
-        String result = Cipher.encrypt("java", 15);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text, then click enter and enter shift.");
+        String result = Cipher.encrypt(scanner.nextLine(), scanner.nextInt());
         System.out.println(result);
+        String decryptResult = Cipher.decrypt(result, scanner.nextInt());
+        System.out.println(decryptResult);
     }
 }
